878a93f6ef1b6355f704e14c115c99b451dc3630
Multa <multamind@gmail.com>
2025-12-12 04:59:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9461449821-32c1e",
+  "appId": "1:343534721488:web:28444df91ffa3c7ea201d5",
+  "apiKey": "AIzaSyDyeUGpCy9_xTTDPvOr9Zj23aMkflVVA7w",
+  "authDomain": "studio-9461449821-32c1e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "343534721488"
+};
